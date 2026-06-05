5960d7fda60afa13aa932ff0dea7015ac8ddacc2
NT <dakana.0801@gmail.com>
2026-06-05 22:39:06 +0900

    ３点セット

---
+++ Assets/Resources/Script/Object/SavePoint.cs
@@ -0,0 +1,32 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+
+public enum ESavePointType
+{
+    None,
+    YagiPoint1,
+    YagiPoint2,
+    YagiPoint3
+}
+
+public class SavePoint : MonoBehaviour
+{
+    [SerializeField] private ESavePointType savePointType;
+
+    private Vector3 newPos;
+
+    private void Awake()
+    {
+        newPos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            PlayerSaveManager.instance.SavePlayerPosition(newPos, savePointType);
+            Debug.Log("New SavePoint: " + newPos);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Resources/Script/Stage3/DoorAnimation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class TriggerAnimation : MonoBehaviour
+{
+    public Animator animator;
+    public string animationTriggerName = "Door";
+    public string targetTag = "Player";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(targetTag))
+        {
+            animator.SetTrigger(animationTriggerName);
+            if (SoundManager.instance != null)
+            {
+                SoundManager.instance.PlaySFXSound("DoorOpen");
+            }
+        }
+    }
+}
