7a1e93fb892a0b3919063c31669229e52b6fe803
Oonari <cocohas@outlook.jp>
2026-07-03 17:58:59 +0900

    Playerの歩きアニメーション
    歩きだけできるようになりました。向きやカメラなどまだ不備があります

---
+++ Assets/Resources/Script/Oonari/PlayerAnimation_Boss.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
+
+public class PlayerAnimation_Boss : MonoBehaviour
+{
+    public Animator animator;
+    public testmove TestMove;
+
+    Quaternion targetRotation;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float animVelocityX =TestMove.horizontal;
+        animator.SetFloat("velocityX", animVelocityX);
+        animator.SetFloat("speedX", Mathf.Abs(TestMove.horizontal));
+
+        //進行方向に合わせて回転する。
+        if (TestMove.horizontal > 0)
+        {
+            //右
+            targetRotation = Quaternion.Euler(0, 170f, 0);
+            transform.Rotate(new Vector3(0, 0, 0));
+        }
+        else if (TestMove.horizontal < 0)
+        {
+            //左
+            targetRotation = Quaternion.Euler(0, 0f, 0);
+            transform.Rotate(new Vector3(0, 0, 0));
+        }
+        
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 10f);
+    }
+}
+
