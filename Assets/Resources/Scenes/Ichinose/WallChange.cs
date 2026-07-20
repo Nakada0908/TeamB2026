8c111b5ab1a16b0bc504c72fe433a40fd9e11b32
25CI0325-Ichinose <25ci0325@jec.ac.jp>
2026-07-20 12:54:02 +0800

    Bossの変更

--- Assets/Resources/Scenes/Ichinose/WallChange.cs
+++ Assets/Resources/Scenes/Ichinose/WallChange.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class WallChange : MonoBehaviour
 {
+    private bool[] changed;
     public GameObject[] oldWall;
     public GameObject[] newWall;
     public Transform player;
@@ -19,19 +20,44 @@ public class WallChange : MonoBehaviour
             newWall[i].SetActive(false);
             oldWall[i].SetActive(true);
         }
+        //changed = new bool[oldWall.Length];
+
+        //for (int i = 0; i < newWall.Length; i++)
+        //{
+        //    newWall[i].SetActive(false);
+        //    oldWall[i].SetActive(true);
+        //    changed[i] = false;
+        //}
     }
 
     int index = 0;
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    if(index < oldWall.Length)
+        //    {
+        //        ChangeWall(index);
+        //        index++;
+        //    }
+        //}
+        // プレイヤーからボスへの方向
+        Vector3 dir = player.position - boss.position;
+
+        float angle = Mathf.Atan2(dir.z, dir.x) * Mathf.Rad2Deg;
+
+        if (angle < 0)
+            angle += 360f;
+
+        int currentIndex = Mathf.FloorToInt(angle / 22.5f);
+        currentIndex = Mathf.Clamp(currentIndex, 0, oldWall.Length - 1);
+
+        if (!changed[currentIndex])
         {
-            if(index < oldWall.Length)
-            {
-                ChangeWall(index);
-                index++;
-            }
+            ChangeWall(currentIndex);
+            changed[currentIndex] = true;
         }
+
     }
 }
