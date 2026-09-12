575862d24ffa2418b540a44eaf4c8bd4d6f0275a
NT <dakana.0801@gmail.com>
2026-09-12 16:13:03 +0900

    ヒント表示とぬいの位置調整とドアの修正

--- Assets/_TeamB/Script/Stage3/DoorAnimation.cs
+++ Assets/_TeamB/Script/Stage3/DoorAnimation.cs
@@ -6,11 +6,17 @@ public class TriggerAnimation : MonoBehaviour
     public string animationTriggerName = "Door";
     public string targetTag = "Player";
 
+    private void Start()
+    {
+        animator.Play("Take 001", 0, 0f);
+        animator.speed = 0f;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(targetTag))
         {
-            animator.SetTrigger(animationTriggerName);
+            animator.speed = 1f;
             if (SoundManager.instance != null)
             {
                 SoundManager.instance.PlaySFXSound("DoorOpen");
